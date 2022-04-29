8313c9cddbfd39f1f03a78a90d43faa986030794
Jovan Danilovic <jovan.danilovic@htecgroup.com>
2022-04-29 16:18:22 +0200

    feat: added firebase config params to env file

--- src/auth/firebase/auth/index.ts
+++ src/auth/firebase/auth/index.ts
@@ -11,13 +11,13 @@ import {
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBUqvEAkKno_3aANAHvFBIwRuTYOntM_G0',
-  authDomain: 'playtomic-coding-exercise.firebaseapp.com',
-  projectId: 'playtomic-coding-exercise',
-  storageBucket: 'playtomic-coding-exercise.appspot.com',
-  messagingSenderId: '1039381117268',
-  appId: '1:1039381117268:web:a9ebb450fe0e80a32a30c1',
-  measurementId: 'G-XEFFQ2PJTM'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
